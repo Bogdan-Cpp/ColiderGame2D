9037753288d10b1c6b17059937580af342d38686
Bogdan <bogdan1000leitan2007@gmail.com>
2024-11-21 22:42:04 +0200

    new mecanics

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,24 @@ class Cub{
 	int y = 100;
    
    void cubMiscare(){
-     if(IsKeyDown(KEY_W)) y = y - 1;
-	 if(IsKeyDown(KEY_S)) y = y + 1;
-	 if(IsKeyDown(KEY_A)) x = x - 1;
-	 if(IsKeyDown(KEY_D)) x = x + 1;
-	 std::cout << "X: " << x << " Y: " << y << '\n';
+	  if(IsKeyDown(KEY_LEFT_SHIFT)){
+	  if(IsKeyDown(KEY_W)) y = y - 5;
+	  if(IsKeyDown(KEY_S)) y = y + 5;
+	  if(IsKeyDown(KEY_A)) x = x - 5;
+	  if(IsKeyDown(KEY_D)) x = x + 5;
+	}
+	
+	else{
+      if(IsKeyDown(KEY_W)) y = y - 3;
+	  if(IsKeyDown(KEY_S)) y = y + 3;
+	  if(IsKeyDown(KEY_A)) x = x - 3;
+	  if(IsKeyDown(KEY_D)) x = x + 3;
+
+	}
    }
 
    void cubDraw(){
-	 DrawRectangle(x, y, 50, 50, RED);
+	 DrawCircle(x, y, 40, BLACK);
    }
 };
 
@@ -25,23 +34,38 @@ class Enemy{
 	int y;
 
    Enemy(int poz_X, int poz_Y){
-     poz_X = x;
-	 poz_Y = y;
+     x = poz_X;
+	 y = poz_Y;
+   }
+   
+   void draw(){
+	 DrawRectangle(x, y, 30, 70, BLUE);
    }
 };
 
 int main(){
-	bool isActive = false;
+	bool isPaused = false;
 	
-	InitWindow(600, 600, "joc");
+	InitWindow(1600, 900, "ColiderBuilding");
 	SetTargetFPS(60);
     Cub cu;
+	Enemy en(200, 400);
 	
 	while(!WindowShouldClose()){
+		if(IsKeyDown(KEY_E)){
+            isPaused = !isPaused;
+		}
 		BeginDrawing();
+
 		ClearBackground(WHITE);
-        cu.cubMiscare();
-		cu.cubDraw();
+		if(isPaused){
+			DrawText("Press key E to resume the game", 300, 300, 60, BLACK);
+		}
+		else{
+          cu.cubMiscare();
+		  cu.cubDraw();
+		  en.draw();
+		}
 		EndDrawing();
 	}
 	CloseWindow();
